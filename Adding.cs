cc81ecc5aff1127187e58b89170687804745c4fb
gharjare <gharjarechetan@gmail.com>
2022-12-01 19:35:39 +0530

    Add two number

---
+++ Adding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem2
+{
+    public class Adding
+    {
+        public static void Add()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
